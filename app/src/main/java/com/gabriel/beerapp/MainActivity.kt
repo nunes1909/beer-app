a447767325cd925d729d96a11663f0d314b1fc72
Gabriel <gnunes1909@gmail.com>
2022-11-08 19:47:51 -0300

    #PBA-1 feature: Dependências e configurações iniciais

--- app/src/main/java/com/gabriel/beerapp/MainActivity.kt
+++ app/src/main/java/com/gabriel/beerapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.gabriel.beerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gabriel.beerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
 }
