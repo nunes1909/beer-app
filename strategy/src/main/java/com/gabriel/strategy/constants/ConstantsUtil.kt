04d42c5af5e77e72491965c4c1eeecf8a9abdf17
Gabriel <gnunes1909@gmail.com>
2022-11-10 12:13:05 -0300

    #PBA-4 feature: Criando arquivoos de Resource e Constants

---
+++ strategy/src/main/java/com/gabriel/strategy/constants/ConstantsUtil.kt
@@ -0,0 +1,9 @@
+package com.gabriel.strategy.constants
+
+object ConstantsUtil {
+    const val BASE_URL = "https://api.punkapi.com/v2/"
+
+    const val BEERS_URL = "beers"
+
+    const val DATA_BASE_NAME = "beers.db"
+}
