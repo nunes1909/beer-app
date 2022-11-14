7556495faffcd0a5c5a0ff3cc25f3ebfe276fbb9
Gabriel <gnunes1909@gmail.com>
2022-11-14 17:59:47 -0300

    #PBA-14 hotfix: Corrigindo erros no cadastro/login

--- remote/src/main/java/com/gabriel/remote/usuario/dataSource/AutenticaUsuarioDataSourceImpl.kt
+++ remote/src/main/java/com/gabriel/remote/usuario/dataSource/AutenticaUsuarioDataSourceImpl.kt
@@ -5,17 +5,23 @@ import com.gabriel.data.usuario.model.UsuarioData
 import com.gabriel.remote.usuario.validate.ValidaUsuarioFirebase
 import com.gabriel.strategy.resource.ResourceState
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlin.coroutines.suspendCoroutine
 
 class AutenticaUsuarioDataSourceImpl(
     private val firebaseAuth: FirebaseAuth,
     private val validaUser: ValidaUsuarioFirebase
 ) : AutenticaUsuarioDataSource {
     override suspend fun autenticaUsuario(usuario: UsuarioData): ResourceState<Boolean> {
-        return try {
-            val task = firebaseAuth.signInWithEmailAndPassword(usuario.email!!, usuario.senha!!)
-            validaUser.validaTask(task = task, auth = true)
-        } catch (e: IllegalAccessException) {
-            ResourceState.Default(data = false, message = "E-mail ou senha não pode ser vazio.")
+        return suspendCoroutine { continuation ->
+            firebaseAuth.signInWithEmailAndPassword(usuario.email!!, usuario.senha!!)
+                .addOnCompleteListener { task ->
+                    continuation.resumeWith(
+                        Result.success(validaUser.validaTask(task = task, auth = true))
+                    )
+                }
         }
     }
 }
